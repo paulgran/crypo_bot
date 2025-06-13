5168667f17e11f97197fea9592cf84fcf056e1af
paulgran <93615013+paulgran@users.noreply.github.com>
2025-06-13 13:37:46 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ from threading import Thread
 load_dotenv()
 API_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHAT_ID = int(os.getenv("TELEGRAM_CHAT_ID", "0"))
-PRICE_DIFF_THRESHOLD = float(os.getenv("PRICE_DIFF_THRESHOLD", 0.005))
+PRICE_DIFF_THRESHOLD = float(os.getenv("PRICE_DIFF_THRESHOLD", 0.001))
 
 PAIRS = [
     "TRXUSDT", "TRXUSDC", "TRXWETH", "TRXBTT", "TRXJST",
